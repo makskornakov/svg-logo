45d99155787e351158666d5b52e14dd33ad428b7
Art Ginzburg <ginzburg.arthur@gmail.com>
2022-12-09 11:34:11 +0400

    try implementing auto-updated paths in Readme and dist

---
+++ scripts/postbuild.mjs
@@ -0,0 +1,51 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+import filenames from '../filenames.json' assert { type: 'json' };
+
+const args = process.argv.slice(2);
+const workingDirRelative = args[0];
+
+const files = await fs.readdir(workingDirRelative);
+
+const filesInDirectory = files.map((filename) => `./${path.join(workingDirRelative, filename)}`);
+
+const fileContentPrevious = await fs.readFile(filenames.workingFileRelative, { encoding: 'utf-8' });
+let fileContentNew = fileContentPrevious;
+
+filesInDirectory.forEach((filename) => {
+  const relevantRegex = filenames.filenamesRegexes.find((regex) =>
+    new RegExp(regex).test(filename),
+  );
+  if (!relevantRegex) return;
+
+  const previousFilename = fileContentPrevious.match(new RegExp(relevantRegex));
+  if (previousFilename?.[0] && previousFilename[0] === filename) return;
+
+  console.log(`${relevantRegex} => ${filename}`);
+  fileContentNew = fileContentNew.replace(new RegExp(relevantRegex), filename);
+});
+
+await Promise.all([
+  fileContentPrevious !== fileContentNew
+    ? fs.writeFile(filenames.workingFileRelative, fileContentNew)
+    : undefined,
+  fs.rm('./filenames.json'),
+]);
+
+const filenamesToCreateSymlinks = filesInDirectory.filter(filename => path.basename(filename) !== 'index.html');
+await Promise.all(filenamesToCreateSymlinks.map((filename) => {
+  const originalFilename = getOriginalFileName(filename);
+  console.log(`linking '${path.basename(filename)}' to '${originalFilename}'`);
+  return fs.symlink(path.basename(filename), originalFilename);
+}))
+
+/**
+ * `./dist/Logo-1.ec6c2488.svg` => `./dist/Logo-1.svg`;
+ * @param {string} filename
+ */
+function getOriginalFileName(filename) {
+  const splitted = filename.split('.');
+  splitted.splice(-2, 1); // remove the hash part;
+  return splitted.join('.');
+}
